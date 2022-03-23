dd83882ce565a3766f9a11201bb0abde433019dd
Anilkumar18 <32846534+Anilkumar18@users.noreply.github.com>
2022-03-23 20:29:55 +0530

    OS Setup: Updated scripts for disk partition

--- lib/disk_partition.sh
+++ lib/disk_partition.sh
@@ -3,9 +3,29 @@
 
 target_disk=$(diskutil list | awk '/Apple_APFS/ {print $7}')
 
-read -P "Provide the disk space want to allocate for Linux OS in GB(e.g:30g):" $size
+default_disk=$target_disk
+default_size='40g'
 
-diskutil apfs resizeContainer $target_disk $size
+read -p "Click Yes to use the default disk for the partition, No to select custom disk (Yes\No):" choice
+
+case "$choice" in
+	Yes|yes|"") Input=1;;
+	No|no) Input=0;;
+	* ) { echo "Invalid option. Plesase select the correct option."; exit 1; };;
+esac
+
+if [ "$target_disk" == 1 ]
+then
+  echo "Resizing the APFS container for partition $default_disk $default_size"
+  diskutil apfs resizeContainer $default_disk $default_size
+fi
+
+if [ "$target_disk" == 0 ]
+then
+  read -P "Provide the disk space want to use for Linux OS:" "$disk"
+  read -P "Provide the disk space want to allocate for Linux OS in GB(e.g:30g):" "$size"
+  diskutil apfs resizeContainer "$disk" "$size"
+fi
 
 disk=$(/dev/disk0)
 
