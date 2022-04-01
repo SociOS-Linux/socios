b521dee3b9db8251f8d3311cceb2db4fbd70f150
Anilkumar18 <32846534+Anilkumar18@users.noreply.github.com>
2022-04-01 21:42:41 +0530

    Socios-setup: Updated code for booting the ISO into the hard disk

--- lib/create_vm.sh
+++ lib/create_vm.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
-Server_name="socios_vm"
-Disk_location="/tmp/socios/$Server_name.vdi"
-Image_file="/tmp/socios/ubuntu.iso"
+MACHINENAME="socios_vm_test"
+#Disk_location="/tmp/socios/$Server_name.vdi"
+#Image_file="/tmp/socios/ubuntu.iso"
 
-# Download debian.iso
-if [ ! -f ./debian.iso ]; then
-    wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-9.9.0-amd64-netinst.iso -O debian.iso
+# Download ubuntu.iso
+if [ ! -f ./ubuntu.iso ]; then
+    echo "Started downloading ubuntu iso image"
+    wget https://releases.ubuntu.com/bionic/ubuntu-18.04.6-desktop-amd64.iso -O ubuntu.iso
 fi
 
+echo "Finished downloading ubuntu iso"
+
 #Creating virtual machine
-vboxmanage createvm --name $Server_name --ostype "Ubuntu_64" --register --basefolder `pwd`
+echo "Creating a $MACHINENAME virtual machine"
+vboxmanage createvm --name $MACHINENAME --ostype "Ubuntu_64" --register --basefolder `pwd`
 
 #Set memory and network
+echo "Setting up the memory and network for created $MACHINENAME virtual machine"
 VBoxManage modifyvm $MACHINENAME --ioapic on
 VBoxManage modifyvm $MACHINENAME --memory 4096
-vboxmanage modifyvm $Server_name --vram 128
+vboxmanage modifyvm $MACHINENAME --vram 128
 VBoxManage modifyvm $MACHINENAME --nic1 nat
-vboxmanage modifyvm $Server_name --cpus 4
-vboxmanage modifyvm $Server_name --graphicscontroller VMSVGA
+vboxmanage modifyvm $MACHINENAME --cpus 4
+vboxmanage modifyvm $MACHINENAME --graphicscontroller VMSVGA
+
+diskutil list
+echo "Select the partition disk to install the OS"
+
+read -p "Provide the disk space want to use for Linux OS:  " disk_input
+
+disk=$(/dev/"$disk_input")
 
 #Create Disk and connect Debian Iso
-VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME.vdi --size 40000 --format VDI
-VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none
+# VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME.vdi --size 40000 --format VDI
+VBoxManage internalcommands createrawvmdk -filename `pwd`/"$MACHINENAME"/$MACHINENAME.vmdk -rawdisk disk
+VBoxManage modifyvm "$MACHINENAME" --boot1 dvd --boot2 disk --boot3 none
 
 #Configuration of virtual hard_disk
-VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storagectl "$MACHINENAME" --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
 
 #Configuration of ISO_image
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
-VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/debian.iso
+VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/ubuntu.iso
 
 #Enable RDP
 VBoxManage modifyvm $MACHINENAME --vrde on
