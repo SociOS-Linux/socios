3a24d416810aba27f0416cf170fd62bdabed927f
Raghul-aitech <raghul.aitech@gmail.com>
2022-04-21 12:13:14 +0530

    Updated ISO image ID & Custom ISO image Dirve link

--- lib/image.sh
+++ lib/image.sh
@@ -4,6 +4,6 @@ echo "Downloading Ubuntu 20.04 unattended installation image."
 
 mkdir -p /tmp/socios
 
-wget -nv --show-progress --load-cookies /tmp/socios/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/socios/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZiqnQn_Ddp1-SQQvqbA2eequ2te6hV2N' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ZiqnQn_Ddp1-SQQvqbA2eequ2te6hV2N" -O /tmp/socios/ubuntu.iso && rm -rf /tmp/socios/cookies.txt
+wget -nv --show-progress --load-cookies /tmp/socios/cookies.txt "https://drive.google.com/u/0/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/socios/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/u/0/uc?export=download&id=1Cfp4J5g-LHkg-g992fTOYxU8MfzmyviL' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Cfp4J5g-LHkg-g992fTOYxU8MfzmyviL" -O /tmp/socios/ubuntu.iso && rm -rf /tmp/socios/cookies.txt
 
 echo "Downloading finished."
