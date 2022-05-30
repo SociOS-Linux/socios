21332e446e22f72c2a47be6dbc2025f4a371daa4
Anilkumar18 <32846534+Anilkumar18@users.noreply.github.com>
2022-05-30 23:01:29 +0530

    Socios: Home brew installer partition function should print the current partition list so that the user can see what them as is #3

--- lib/disk_partition.sh
+++ lib/disk_partition.sh
@@ -16,10 +16,9 @@ default_size=50
 read -p "Enter Yes to use the default disk for the partition, No to select custom disk (Yes\No):" choice
 
 case "$choice" in
-      Yes|yes|y|Y|YES|"") Input=1;;
-      No|no|n|N|NO|"") Input=0;;
-      * ) { echo "Invalid option. Please select the correct option."; exit 1; };;
-      No|no) Input=0;;
+	Yes|yes|"") Input=1;;
+	No|no) Input=0;;
+	* ) { echo "Invalid option. Please select the correct option."; exit 1; };;
 esac
 
 if [ $Input == 1 ]
@@ -29,50 +28,52 @@ then
     crct_default_space="$(echo $total_space - $default_size | bc)"
     sudo diskutil apfs resizeContainer "$default_disk" "$crct_default_space""g"
 else
-    diskutil list
-    echo "Provide the disk space want to use for the above list."
-  total_space="$(diskutil info /dev/$default_disk | grep "Disk Size" | awk '{print $3}')"
-    read -p "Provide the disk space want to allocate for Linux OS in numerals(e.g:30 for 30GB):  " size_input
-  if [[ $size_input -le 20 ]]; then
-    read -p "Please Provide the disk space more than 20(e.g:30):  " size_input
-    crct_space="$(echo $total_space - $size_input | bc)"
-    diskutil apfs resizeContainer "$default_disk" "$crct_space""g"
-  else
-    crct_space="$(echo $total_space - $size_input | bc)"
-    diskutil apfs resizeContainer "$default_disk" "$crct_space""g"
-  fi
+    diskutil list $default_disk
+	space_available=$(diskutil info / | grep "Container Free Space" | awk '{print $4$5}')
+	echo "You have $space_available of free space in your Mac machine"
+    echo "Provide the disk space want to use from the above list."
+	total_space="$(diskutil info /dev/$default_disk | grep "Disk Size" | awk '{print $3}')"
+    read -p "Provide the disk space want to allocate for Linux OS in numerals(e.g:40 for 40GB):  " size_input
+	if [[ $size_input -le 30 ]]; then
+		read -p "Please Provide the disk space more than 30(e.g:40):  " size_input
+		crct_space="$(echo $total_space - $size_input | bc)"
+		diskutil apfs resizeContainer "$default_disk" "$crct_space""g"
+	else
+		crct_space="$(echo $total_space - $size_input | bc)"
+		diskutil apfs resizeContainer "$default_disk" "$crct_space""g"
+	fi
 fi
 
-
-#diskutil info /dev/disk1s2 | grep "Part of Whole" | awk '{print $4}'
-disk_identifier="$(diskutil info /dev/$default_disk | grep "Part of Whole" | awk '{print $4}')"
-whole_disk=/dev/"$disk_identifier"
-
-echo "Installing SGdisk"
 brew install gptfdisk
 
-echo "SGDisk installation completed"
+#diskutil info /dev/disk1s2 | grep "Part of Whole" | awk '{print $4}'
+parent_identifier="$(diskutil info /dev/$default_disk | grep "Part of Whole" | awk '{print $4}')"
+whole_disk=/dev/"$parent_identifier"
 
 ram=$(system_profiler SPHardwareDataType | grep "Memory:" | awk '{print $2}')
 
 if [[ $ram -le 12 ]]
 then
     let ram=ram
-  sum=$(expr $ram + 4)
-  extra_ram="+""$sum""G"
-  echo "Swap space of $sum"GB" is created for Socios"
-  sudo sgdisk -n 0:0:$extra_ram -c 0:"SWAP" -t 0:0700 "$whole_disk"
+	sum=$(expr $ram + 2)
+	extra_ram="+""$sum""G"
+	echo "Swap space of $sum"GB" is created for Socios"
+	sudo sgdisk -n 0:0:$extra_ram -c 0:"SWAP" -t 0:8200 "$whole_disk"
 else
     let ram=12
-  sum=$(expr $ram + 4)
-  extra_ram="+""$sum""G"
-  echo "Swap space of $sum"GB" is created for Socios"
-  sudo sgdisk -n 0:0:$extra_ram -c 0:"SWAP" -t 0:0700 "$whole_disk"
+	sum=$(expr $ram + 2)
+	extra_ram="+""$sum""G"
+	echo "Swap space of $sum"GB" is created for Socios"
+	sudo sgdisk -n 0:0:$extra_ram -c 0:"SWAP" -t 0:8200 "$whole_disk"
 fi
+
+sudo sgdisk -n 0:0:+2M -c 0:"Boot partition" -t 0:ef02  "$whole_disk"
+
+sudo sgdisk -n 0:0:+3G -c 0:"Linux FS" -t 0:8300 "$whole_disk"
 
 echo "Rest of the space is allotted for the Socios Storage and booting process"
 ENDSECTOR=$(sudo sgdisk -E "$whole_disk")
-sudo sgdisk -n 0:0:"$ENDSECTOR" -c 0:"FEDORA" -t 0:0700 "$whole_disk"
+sudo sgdisk -n 0:0:"$ENDSECTOR" -c 0:"Linux FS" -t 0:8300 "$whole_disk"
 sudo sgdisk -p "$whole_disk"
 
 diskutil list
